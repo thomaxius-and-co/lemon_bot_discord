4cfb44f4e64899361eabd7d97123f4f682bd0322
Henry Heikkinen <rce@rce.fi>
2016-12-01 19:26:29 +0200

    Refactor database.py

--- src/database.py
+++ src/database.py
@@ -1,29 +1,33 @@
 import os
+from contextlib import contextmanager
 
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
-def db_connect():
-    return psycopg2.connect("host=localhost dbname=%s user=%s password=%s" % (
+@contextmanager
+def connect():
+    connect_string = "host=localhost dbname=%s user=%s password=%s" % (
         os.environ["DATABASE_NAME"],
         os.environ["DATABASE_USERNAME"],
         os.environ["DATABASE_PASSWORD"]
-    ))
+    )
+    with psycopg2.connect(connect_string, cursor_factory=RealDictCursor) as con:
+        with con.cursor() as c:
+            yield c
 
 def initialize_schema():
-    with db_connect() as conn:
-        with conn.cursor() as c:
-            c.execute("""
-                CREATE TABLE IF NOT EXISTS start_times (
-                    ts timestamp NOT NULL,
-                    message TEXT
-                );
-            """)
+    with connect() as c:
+        c.execute("""
+            CREATE TABLE IF NOT EXISTS start_times (
+                ts timestamp NOT NULL,
+                message TEXT
+            );
+        """)
 
 def insert_start_time(message):
-    with db_connect() as conn:
-        with conn.cursor() as c:
-            c.execute("""
-                INSERT INTO start_times (ts, message)
-                VALUES (current_timestamp, %s)
-            """, [message])
+    with connect() as c:
+        c.execute("""
+            INSERT INTO start_times (ts, message)
+            VALUES (current_timestamp, %s)
+        """, [message])
 
