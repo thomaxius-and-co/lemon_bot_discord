14d636a81ca65bd2b899ac0e3cb1e686c03f662a
Henry Heikkinen <rce@rce.fi>
2016-12-10 20:08:37 +0200

    Use feedparser to parse feeds
    Everything works so much better

--- src/feed.py
+++ src/feed.py
@@ -1,15 +1,18 @@
 from asyncio import sleep
-from dateutil import parser
 import aiohttp
 from datetime import datetime
-import pytz
+#import pytz
 import time
 import traceback
 import re
 from bs4 import BeautifulSoup
 import discord
+import feedparser
 from database import connect
 import emoji
+
+def time_to_datetime(struct_time):
+    return datetime.fromtimestamp(time.mktime(struct_time))
 
 def initialize_schema():
     with connect() as c:
@@ -29,57 +32,18 @@ async def check_feeds(client):
         feeds = c.fetchall()
 
     for id, url, last_entry, channel_id in feeds:
-        await process_feed(client, id, url, pytz.utc.localize(last_entry), channel_id)
+        await process_feed(client, id, url, last_entry, channel_id)
 
-def parse_rss_item(item):
-    title = item.find("title").get_text()
-    date = parser.parse(item.find("pubdate").get_text())
-    url = ""
-    if item.find("guid") and item.find("guid").get_text():
-        url = item.find("guid").get_text()
-    if item.find("link") and item.find("link").get_text():
-        url = item.find("link").get_text()
+def get_new_items(url, since):
+    def parse_entry(e):
+        return {
+            "title": e.title,
+            "url": e.links[0].href,
+            "date": time_to_datetime(e.published_parsed),
+        }
+    d = feedparser.parse(url)
+    return d.feed.title, d.feed.link, [e for e in map(parse_entry, d.entries) if e["date"] > since]
 
-    return {
-        "title": title,
-        "date": date,
-        "url": url,
-    }
-
-def parse_atom_item(item):
-    title = item.find("title").get_text()
-    date = parser.parse(item.find("updated").get_text())
-    url = item.find("link").get("href")
-
-    return {
-        "title": title,
-        "date": date,
-        "url": url,
-    }
-
-async def get_feed_xml(url):
-    async with aiohttp.get(url) as r:
-        if r.status != 200:
-            print("ERROR: status {0}".format(r.status))
-            return None
-        return BeautifulSoup(await r.text(), "lxml")
-
-async def get_new_items(url, since):
-    def get_title(xml):
-        root = xml.find("channel")
-        if root is None:
-            root = xml.find("feed")
-        return root.find("title").get_text()
-
-    xml = await get_feed_xml(url)
-    if xml is None:
-        return None, []
-
-    items = list(map(parse_rss_item, xml.find_all('item')))
-    if len(items) == 0:
-        items = map(parse_atom_item, xml.find_all('entry'))
-
-    return get_title(xml), [item for item in items if item["date"] > since]
 
 def make_embed(item):
     embed = discord.Embed(title = item["title"], url = item["url"])
@@ -88,12 +52,12 @@ def make_embed(item):
 
 async def process_feed(client, id, url, last_entry, channel_id):
     print("feed: processing feed '{0}'".format(url))
-    title, new_items = await get_new_items(url, last_entry)
+    feed_title, feed_url, new_items = get_new_items(url, last_entry)
     if len(new_items) > 0:
         # Send messages
         for item in new_items:
             embed = make_embed(item)
-            embed.set_author(name=title)
+            embed.set_author(name=feed_title, url=feed_url)
             await client.send_message(discord.Object(id=channel_id), embed=embed)
 
         # Update last entry
