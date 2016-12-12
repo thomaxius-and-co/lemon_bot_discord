dd03bed56e6e1886293ab5df584a63a9cae889e4
Henry Heikkinen <rce@rce.fi>
2016-12-12 21:06:31 +0200

    Refactor !osu command

--- src/osu.py
+++ src/osu.py
@@ -3,61 +3,7 @@ import json
 import aiohttp
 
 import command
-
-def make_query_string(params):
-    return "?" + "&".join(map(lambda x: "=".join(x), params.items()))
-
-async def call_api(endpoint, params):
-    url = "https://osu.ppy.sh/api/%s%s" % (endpoint, make_query_string(params))
-    async with aiohttp.get(url) as r:
-        return await r.json()
-
-async def get_user(name):
-    return await call_api("get_user", {
-        "k": os.environ["OSU_API_KEY"],
-        "type": "u",
-        "u": name,
-        "event_days": "1",
-    })
-
-async def get_user_best(name, limit):
-    if not (1 <= limit <= 100):
-        raise Error("osu: invalid limit")
-
-    return await call_api("get_user_best", {
-        "k": os.environ["OSU_API_KEY"],
-        "type": "u",
-        "u": name,
-        "limit": str(limit),
-    })
-
-async def beatmap(id):
-    beatmaps = await call_api("get_beatmaps", {
-        "k": os.environ["OSU_API_KEY"],
-        "m": "0",
-        "b": str(id),
-        "limit": "1",
-    })
-    return beatmaps[0] if len(beatmaps) > 0 else None
-
-def format_pp(play):
-    return round(float(play["pp"]))
-
-def format_score(play):
-    score = int(play["score"])
-    if score > 1000000:
-        return "%sM" % round(score / 1000000.0, 1)
-    if score > 1000:
-        return "%sk" % round(score / 1000.0, 1)
-    return "%s" % score
-
-def format_accuracy(play):
-    hits = 0
-    hits += int(play["count50"]) * 50
-    hits += int(play["count100"]) * 100
-    hits += int(play["count300"]) * 300
-    max_hits = (int(play["count50"]) + int(play["count100"]) + int(play["count300"]) + int(play["countmiss"])) * 300
-    return round(float(hits) / float(max_hits) * 100, 2)
+import osu_api as api
 
 async def cmd_osu(client, message, arg):
     if arg is None:
@@ -76,79 +22,35 @@ async def cmd_osu(client, message, arg):
         await cmd_osu_user(client, message, cmd)
 
 async def cmd_osu_user(client, message, user):
-    results = await get_user(user)
+    results = await api.user(user)
     if not results:
         await client.send_message(message.channel, "User %s not found" % user)
         return
 
-    data = results[0]
-    username = data["username"]
-    rank = int(data["pp_rank"])
-    pp = round(float(data["pp_raw"]))
-    acc = float(data["accuracy"])
-
-    reply = "%s (#%d) has %d pp and %.2f%% acc" % (username, rank, pp, acc)
+    reply = "{user.username} (#{user.rank}) has {user.pp_rounded} pp and {user.accuracy_rounded}% acc".format(user=next(results))
     await client.send_message(message.channel, reply)
 
-def format_mods(mods):
-    bitmasks = [
-        ("NF", 1 << 0),
-        ("EZ", 1 << 1),
-        ("HD", 1 << 3),
-        ("HR", 1 << 4),
-        ("SD", 1 << 5),
-        ("DT", 1 << 6),
-        ("RX", 1 << 7),
-        ("HT", 1 << 8),
-        ("NC", 1 << 9),
-        ("FL", 1 << 10),
-        ("SO", 1 << 12),
-        ("PF", 1 << 14),
-    ]
-
-    s = ""
-    for name, mask in bitmasks:
-        print(name, mask)
-        if mods & mask:
-            s += name
-    return (s + " ").lstrip(" ")
-
 async def cmd_osu_best(client, message, user):
-    results = await get_user_best(user, 5)
+    results = await api.user_best(user, 5)
     if not results:
         await client.send_message(message.channel, "User %s not found" % user)
         return
 
     plays = []
     for i, play in enumerate(results):
-        beatmap_id = play["beatmap_id"]
-        bm = await beatmap(beatmap_id)
-        if bm is not None:
-            beatmap_name = "{artist} - {title} [{version}]".format(**bm)
-            stars = round(float(bm["difficultyrating"]), 2)
-        else:
-            beatmap_name = "#" + beatmap_id
-            stars = "?.??"
-
-        info = {
-            "index": i + 1,
-            "beatmap_name": beatmap_name,
-            "stars": stars,
-            "rank": play["rank"],
-            "mods": format_mods(int(play["enabled_mods"])),
-            "score": format_score(play),
-            "pp": format_pp(play),
-            "acc": format_accuracy(play),
-            "date": play["date"],
-            "combo": int(play["maxcombo"]),
-            "fc": "" if play["perfect"] == "0" else " FC",
-        }
+        bm = await play.beatmap()
 
         template = "\n".join([
-            "{index}. {beatmap_name} (★{stars})",
-            "   {mods}{rank} {acc}% {combo}x{fc} {pp}pp, {score} score ({date})",
+            "{index}. {bm.artist} - {bm.title} [{bm.version}] (★{bm.stars_rounded})",
+            "   {mods}{play.rank} {play.accuracy_rounded}% {play.combo}x{full_combo} {play.pp_rounded}pp, {play.score_formatted} score ({play.date})",
         ])
-        plays.append(template.format(**info))
+        plays.append(template.format(
+            index = i+1,
+            bm = bm,
+            play = play,
+            full_combo = " FC" if play.full_combo else "",
+            mods = (play.mods + " ").lstrip(" "),
+        ))
 
     reply = "```%s```" % "\n".join(plays)
     await client.send_message(message.channel, reply)
---
+++ src/osu_api.py
@@ -0,0 +1,133 @@
+import os
+import aiohttp
+
+class User:
+    def __init__(self, json):
+        self.json = json
+        self.username = json["username"]
+        self.rank = int(json["pp_rank"])
+        self.pp = float(json["pp_raw"])
+        self.pp_rounded = round(self.pp)
+        self.accuracy = float(json["accuracy"])
+        self.accuracy_rounded = round(self.accuracy, 2)
+
+        self._best = None
+
+    @property
+    async def best(self):
+        if self._best is None:
+            self._best = await user_best(self.username)
+        return self._best
+
+class Play:
+    def __init__(self, json):
+        self.json = json
+        self.beatmap_id = int(json["beatmap_id"])
+        self.full_combo = json["perfect"] != "0"
+        self.rank = json["rank"]
+        self.mods_raw = int(json["enabled_mods"])
+        self.score = int(json["score"])
+        self.pp = float(json["pp"])
+        self.pp_rounded = round(self.pp)
+        self.date = json["date"]
+        self.combo = int(json["maxcombo"])
+
+        self._beatmap = None
+
+    @property
+    def score_formatted(self):
+        if self.score > 1000000:
+            return "{0}M".format(round(self.score / 1000000.0, 1))
+        if self.score > 1000:
+            return "{0}k".format(round(self.score / 1000.0, 1))
+        return "{0}".format(self.score)
+
+    @property
+    def mods(self):
+        bitmasks = [
+            ("NF", 1 << 0),
+            ("EZ", 1 << 1),
+            ("HD", 1 << 3),
+            ("HR", 1 << 4),
+            ("SD", 1 << 5),
+            ("DT", 1 << 6),
+            ("RX", 1 << 7),
+            ("HT", 1 << 8),
+            ("NC", 1 << 9),
+            ("FL", 1 << 10),
+            ("SO", 1 << 12),
+            ("PF", 1 << 14),
+        ]
+
+        s = ""
+        for name, mask in bitmasks:
+            if self.mods_raw & mask:
+                s += name
+        return s
+
+    @property
+    def accuracy(self):
+        count50 = int(self.json["count50"])
+        count100 = int(self.json["count100"])
+        count300 = int(self.json["count300"])
+        countmiss = int(self.json["countmiss"])
+        hits = 0
+        hits += count50 * 50
+        hits += count100 * 100
+        hits += count300 * 300
+        max_hits = (count50 + count100 + count300 + countmiss) * 300
+        return float(hits) / float(max_hits) * 100
+
+    @property
+    def accuracy_rounded(self):
+        return round(self.accuracy, 2)
+
+    async def beatmap(self):
+        if self._beatmap is None:
+            self._beatmap = next(await beatmaps(self.beatmap_id))
+        return self._beatmap
+
+class Beatmap:
+    def __init__(self, json):
+        self.json = json
+        self.beatmap_id = json["beatmap_id"]
+        self.artist = json["artist"]
+        self.title = json["title"]
+        self.version = json["version"]
+        self.stars = float(json["difficultyrating"])
+        self.stars_rounded = round(self.stars, 2)
+
+def make_query_string(params):
+    return "?" + "&".join(map(lambda x: "=".join(x), params.items()))
+
+async def call_api(endpoint, params):
+    url = "https://osu.ppy.sh/api/%s%s" % (endpoint, make_query_string(params))
+    async with aiohttp.get(url) as r:
+        return await r.json()
+
+async def user(name):
+    return map(User, await call_api("get_user", {
+        "k": os.environ["OSU_API_KEY"],
+        "type": "u",
+        "u": name,
+        "event_days": "1",
+    }))
+
+async def user_best(name, limit):
+    if not (1 <= limit <= 100):
+        raise Error("osu: invalid limit")
+
+    return map(Play, await call_api("get_user_best", {
+        "k": os.environ["OSU_API_KEY"],
+        "type": "u",
+        "u": name,
+        "limit": str(limit),
+    }))
+
+async def beatmaps(beatmap_id):
+    return map(Beatmap, await call_api("get_beatmaps", {
+        "k": os.environ["OSU_API_KEY"],
+        "m": "0",
+        "b": str(beatmap_id),
+        "limit": "1",
+    }))
