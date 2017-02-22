de8cd1d421c66db53d9f64b7f3506fe9169407eb
Henry Heikkinen <rce@rce.fi>
2017-02-22 15:32:11 +0200

    Add foreign key constraints to make sure references are valid

--- src/database.py
+++ src/database.py
@@ -182,6 +182,27 @@ schema_migrations = {
             PRIMARY KEY (user_id)
         );
     """,
+
+    # Add foreign key constraints to make sure data references are valid
+    11: """
+        ALTER TABLE casino_account
+        ADD FOREIGN KEY (user_id) REFERENCES discord_user (user_id);
+
+        ALTER TABLE casino_bet
+        ADD FOREIGN KEY (user_id) REFERENCES discord_user (user_id);
+
+        ALTER TABLE reminder
+        ADD FOREIGN KEY (user_id) REFERENCES discord_user (user_id);
+
+        ALTER TABLE casino_stats
+        ADD FOREIGN KEY (user_id) REFERENCES discord_user (user_id);
+
+        ALTER TABLE whosaidit_stats
+        ADD FOREIGN KEY (user_id) REFERENCES discord_user (user_id);
+
+        ALTER TABLE channel_archiver_status
+        ADD FOREIGN KEY (message_id) REFERENCES message (message_id);
+    """,
 }
 
 _pool_holder = threading.local()
