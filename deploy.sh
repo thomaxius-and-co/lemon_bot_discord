62b8a3072cad09c1061b1513af38b6480b916514
Henry Heikkinen <rce@rce.fi>
2017-03-05 16:32:54 +0200

    Enable DigitalOcean monitoring agent during droplet creation
    Use forked digital_ocean ansible module and dopy library to use the new
    DigitalOcean API parameter to enable monitoring agent so we don't have
    to manually install it.
    
    Contributing to ansible once the [dopy pull request][1] is merged and
    published.
    
    [1]: https://github.com/Wiredcraft/dopy/pull/57

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd $(dirname "${BASH_SOURCE[0]}")
+ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+cd $ROOT
 
 set -e
 trap "rm vault.pw" SIGINT SIGTERM EXIT
 
 echo $ANSIBLE_VAULT_PASSWORD > vault.pw
+
+ANSIBLE_LIBRARY="$ROOT/ansible/library" \
 ansible-playbook \
   --vault-password-file=vault.pw \
   --extra-vars=@ansible/secrets.yml \
