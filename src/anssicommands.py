3390e985a52d1dc8f008c995674cf64f98cfe7ca
Henry Heikkinen <rce@rce.fi>
2017-04-26 13:16:53 +0300

    Removed unnecessary loops

--- src/anssicommands.py
+++ src/anssicommands.py
@@ -5,14 +5,10 @@ import asyncio
 
 choices=["Todellakin","Kyllä","Lopeta heti kyseleminen","Ehkä","Todennäköisesti ei","Ei","Ei todellakaan","En tiedä","Juuh joo","Juuh ei","Varmasti","Lol ei"]
 
-
 async def cmd_ask(client, message, _):
-    while True:
-        for i in range(0,1):
-            await client.send_message(message.channel, " :thinking: ")
-            await asyncio.sleep(2)
-        await client.send_message(message.channel, random.choice(choices))
-        break
+    await client.send_message(message.channel, " :thinking: ")
+    await asyncio.sleep(2)
+    await client.send_message(message.channel, random.choice(choices))
 
 def register(client):
     return {
