4b46ae99f25726669f5b8e27bdf220482bc2eb4a
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-04-26 13:04:07 +0300

    use asyncio

--- src/anssicommands.py
+++ src/anssicommands.py
@@ -1,6 +1,7 @@
 # Anssi's first command!!
 import time
 import random
+import asyncio
 
 choices=["Todellakin","Kyllä","Lopeta heti kyseleminen","Ehkä","Todennäköisesti ei","Ei","Ei todellakaan","En tiedä","Juuh joo","Juuh ei","Varmasti","Lol ei"]
 
@@ -9,7 +10,7 @@ async def cmd_ask(client, message, _):
     while True:
         for i in range(0,1):
             await client.send_message(message.channel, " :thinking: ")
-            time.sleep(2)
+            await asyncio.sleep(2)
         await client.send_message(message.channel, random.choice(choices))
         break
 
