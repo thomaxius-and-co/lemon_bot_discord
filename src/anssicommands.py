5fd002b9839c7f183b1efa16c26220fb11488c77
Anpsi <anssi.lainepaa@gmail.com>
2017-04-28 14:56:32 +0300

    Making !ask great
    Added more choices and made !ask answers actually fit the question

--- src/anssicommands.py
+++ src/anssicommands.py
@@ -3,10 +3,23 @@ import time
 import random
 import asyncio
 
-choices=["Todennäköisesti kyllä","Kyllä","Ei","Todennäköisesti ei","Ei todellakaan","En tiedä","Jooh","Juuh ei","Varmasti","Lol ei"]
+default_choices =["Kyllä", "Ei", "Ehkä"]
+is_choices =["On", "Ei ole" , "Ehkä"]
+where_choices =["Koska perseestä tulee paskaa", "Koska maailma ei ole valmis", "Koska botti on rikki"]
+when_choices =["Viimeksi joulukuussa" , "Sitten kun anps voi hyvin" , "Ei vittu ikinä"]
 
-async def cmd_ask(client, message, _):
-    await client.send_message(message.channel)
+async def cmd_ask(client, message, question):
+    if not question:
+        await client.send_message(message.channel, 'kys pls')
+        return
+    if question.lower().startswith("onko"):
+        choices = is_choices
+    elif question.lower().startswith("mistä"):
+        choices = where_choices
+    elif question.lower().startswith("milloin"):
+        choices = when_choices
+    else:
+        choices = default_choices        
     await asyncio.sleep(2)
     await client.send_message(message.channel, random.choice(choices) + " :thinking: ")
 
