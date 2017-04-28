d6176e83e3c676022b7138e99fc86b8661d77f4c
Anpsi <anssi.lainepaa@gmail.com>
2017-04-28 17:08:47 +0300

    !ask
    Added more choices

--- src/anssicommands.py
+++ src/anssicommands.py
@@ -3,12 +3,22 @@ import time
 import random
 import asyncio
 
-default_choices =["Kyllä", "Ei", "Ehkä"]
-is_choices =["On", "Ei ole" , "Ehkä"]
-from_choices =["Pyllystä", "Sun mutsista", "Sossusta", "Kaupasta", "No vittu just sieltä", "Säkylästä"]
-when_choices =["Viimeksi joulukuussa" , "Sitten kun anps voi hyvin" , "Ei vittu ikinä"]
-why_choices =["Koska säkylän pystykorvat" , "Kukaan ei tiedä" , "Se on jumalan suunnitelma"]
-where_choices =["Helvetin perseessä" , "Jumalan selän takana" , "Nyrkki perseessä thaimaassa"]
+default_choices =["Kyllä", "Ei", "Ehkä" , "Öööö.." , "Hmm.." , "Mjaa.." , "..." , "En osaa vastata tuohon"]
+is_choices =["On", "Ei ole" , "Ehkä" , "Kyllä varmaan" , "Ei varmaan" , "Todellakin" , "Ei todellakaan"]
+from_choices =["Pyllystä", "Sun mutsista", "Sossusta", "Kaupasta", "No vittu just sieltä", "Säkylästä" , "Stadista" , "Ei mistään"]
+when_choices =["Viimeksi joulukuussa" , "Sitten kun anps voi hyvin" , "Ei vittu ikinä" , "Aikavälillä 10,000ekr - 2017"]
+why_choices =["Koska säkylän pystykorvat" , "Kukaan ei tiedä" , "Se on jumalan suunnitelma" , "Tuntemattomista syistä"]
+where_choices =["Helvetin perseessä" , "Jumalan selän takana" , "Nyrkki perseessä säkylässä" , "Leppävaarassa"]
+who_choices =["Helvetin perkeleet" , "Niske" , "Henry" , "Chimppa" , "Tuntematon mieshenkilö" , "Anps" , "Tommi" , "En tiedä"]
+how_choices =["Katso googlesta" , "En tiedä enkä osaa" , "En voi auttaa asian kanssa" , "Hääv juu traid rebooting?" , "Kynsisaksilla"]
+what_choices =["Kasa paskaa" , "Kuuku vitun pööpötin" , "Puhdas ja sileä honkanen" , "Reaktorin tesla" , "Anime"]
+whatlike_choices =["Ruosteinen" , "Paskainen" , "Jostain syystä omituisen hyväntuoksuinen" , "Jännä" , "Pullea" , "Söpö"]
+areyou_choices =["Kyllä olen", "En voi kertoa" , "En" , "Se on salaisuus"]
+arewe_choices =["Olette" , "Ette ole" , "En voi kertoa" , "Se on salaisuus"]
+arethey_choices =["Ovat" , "Ei ole" , "En kerro"]
+worldDomination_choices =["En kerro" , "Huomenna tai ylihuomenna" , "16.09.2028" , "23.04.2026" , "Viikon sisällä" , "Tänään"]
+rain_choices =["Sataa ihan vitusti" , "Ei mutta paskaa tulee taivaalta" , "Joo, voi tulla vettä niskaan" , "Ei sada"]
+weather_choices =["En osaa ennustaa" , "Puolipilvistä" , "Epämääräinen" , "Kertakaikkiaan ihan vitun paska keli" , "Lol ihanku menisit ulos"]
 
 async def cmd_ask(client, message, question):
     if not question:
@@ -25,6 +35,26 @@ async def cmd_ask(client, message, question):
         choices = why_choices
     elif question.lower().startswith("missä"):
         choices = where_choices
+    elif question.lower().startswith("kuka"):
+        choices = who_choices
+    elif question.lower(.startswith("miten") or question.lower().startswith("kuinka"):
+        choices = how_choices
+    elif question.lower(.startswith("mikä"):
+        choices = what_choices
+    elif question.lower(.startswith("millainen") or question.lower().startswith("minkälainen"):
+        choices = whatlike_choices
+    elif question.lower(.startswith("oletko"):
+        choices = areyou_choices
+    elif question.lower(.startswith("olemmeko"): 
+        choices = arewe_choices               
+    elif question.lower(.startswith("ovatko"):                    
+        choices = arethey_choices                
+    elif question.lower(.startswith("milloin valloitat")
+        choices = worldDomination_choices
+    elif question.lower(.startswith("sataako"):                    
+        choices = rain_choices                
+    elif question.lower(.startswith("millainen sää"):
+        choices = weather_choices                
     else:
         choices = default_choices
 
