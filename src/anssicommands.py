98ca7a62949262170a0b50cda9527f2938738afa
Anpsi <anssi.lainepaa@gmail.com>
2017-04-30 14:55:50 +0300

    Update anssicommands.py
    + should_choices
    + fixes

--- src/anssicommands.py
+++ src/anssicommands.py
@@ -3,22 +3,23 @@ import time
 import random
 import asyncio
 
-default_choices =["Kyllä", "Ei", "Ehkä" , "Öööö.." , "Hmm.." , "Mjaa.." , "..." , "En osaa vastata tuohon"]
+default_choices =["Kyllä", "Ei", "Ehkä" , "Hmm.." , "Mjaa.." , "En osaa vastata tuohon"]
 is_choices =["On", "Ei ole" , "Ehkä" , "Kyllä varmaan" , "Ei varmaan" , "Todellakin" , "Ei todellakaan"]
 from_choices =["Pyllystä", "Sun mutsista", "Sossusta", "Kaupasta", "No vittu just sieltä", "Säkylästä" , "Stadista" , "Ei mistään"]
-when_choices =["Viimeksi joulukuussa" , "Sitten kun anps voi hyvin" , "Ei vittu ikinä" , "Aikavälillä 10,000ekr - 2017"]
+when_choices =["Viimeksi joulukuussa" , "Ei vittu ikinä" , "Aikavälillä 10,000ekr - 2068" , "Vuonna 85"]
 why_choices =["Koska säkylän pystykorvat" , "Kukaan ei tiedä" , "Se on jumalan suunnitelma" , "Tuntemattomista syistä"]
 where_choices =["Helvetin perseessä" , "Jumalan selän takana" , "Nyrkki perseessä säkylässä" , "Leppävaarassa"]
 who_choices =["Helvetin perkeleet" , "Niske" , "Henry" , "Chimppa" , "Tuntematon mieshenkilö" , "Anps" , "Tommi" , "En tiedä"]
 how_choices =["Katso googlesta" , "En tiedä enkä osaa" , "En voi auttaa asian kanssa" , "Hääv juu traid rebooting?" , "Kynsisaksilla"]
 what_choices =["Kasa paskaa" , "Kuuku vitun pööpötin" , "Puhdas ja sileä honkanen" , "Reaktorin tesla" , "Anime"]
-whatlike_choices =["Ruosteinen" , "Paskainen" , "Jostain syystä omituisen hyväntuoksuinen" , "Jännä" , "Pullea" , "Söpö"]
+whatlike_choices =["Ruosteinen" , "Paskainen" , "Jostain syystä pahanhajuinen" , "Jännä" , "Pullea" , "Söpö"]
 areyou_choices =["Kyllä olen", "En voi kertoa" , "En" , "Se on salaisuus"]
 arewe_choices =["Olette" , "Ette ole" , "En voi kertoa" , "Se on salaisuus"]
 arethey_choices =["Ovat" , "Ei ole" , "En kerro"]
 worldDomination_choices =["En kerro" , "Huomenna tai ylihuomenna" , "16.09.2028" , "23.04.2026" , "Viikon sisällä" , "Tänään"]
 rain_choices =["Sataa ihan vitusti" , "Ei mutta paskaa tulee taivaalta" , "Joo, voi tulla vettä niskaan" , "Ei sada"]
 weather_choices =["En osaa ennustaa" , "Puolipilvistä" , "Epämääräinen" , "Kertakaikkiaan ihan vitun paska keli" , "Lol ihanku menisit ulos"]
+should_choices =["Kyllä" , "Ei" , "Ei ikinä" , "Tottakai"]
 
 async def cmd_ask(client, message, question):
     if not question:
@@ -29,7 +30,7 @@ async def cmd_ask(client, message, question):
         choices = is_choices
     elif question.lower().startswith("mistä"):
         choices = from_choices
-    elif question.lower().startswith("milloin"):
+    elif question.lower().startswith("milloin") or question.lower().startswith("millon"):
         choices = when_choices
     elif question.lower().startswith("miksi") or question.lower().startswith("minkä takia"):
         choices = why_choices
@@ -43,18 +44,20 @@ async def cmd_ask(client, message, question):
         choices = what_choices
     elif question.lower().startswith("millainen") or question.lower().startswith("minkälainen"):
         choices = whatlike_choices
-    elif question.lower().startswith("oletko"):
+    elif question.lower().startswith("oletko") or question.lower().startswith("ookko"):
         choices = areyou_choices
     elif question.lower().startswith("olemmeko"): 
         choices = arewe_choices               
     elif question.lower().startswith("ovatko"):                    
         choices = arethey_choices                
-    elif question.lower().startswith("milloin valloitat"):
+    elif question.lower().startswith("maailmanvalloitus"):
         choices = worldDomination_choices
     elif question.lower().startswith("sataako"):                    
         choices = rain_choices                
-    elif question.lower().startswith("millainen sää"):
-        choices = weather_choices                
+    elif question.lower().startswith("sää"):
+        choices = weather_choices
+    elif question.lower().startswith("pitäisikö") or question.lower().startswith("pitäiskö"):
+        choices = should_choices       
     else:
         choices = default_choices
 
