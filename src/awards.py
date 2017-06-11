95f0b512e9621bbf1b3f15ea3daa2e0aded1d44e
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-06-11 14:47:50 +0300

    Cosmetic changes
    Both to back and front

--- src/awards.py
+++ src/awards.py
@@ -13,7 +13,7 @@ async def cmd_trophycabinet(client, message, arg):
     if trophycabinet:
         for trophy in trophycabinet:
             msg += ':trophy:' + trophy + '\n'
-        await client.edit_message(oldmessage, 'Your trophies: \n' + msg + '\n')
+        await client.edit_message(oldmessage, 'Your trophies:\n ' + msg + '\n')
     else:
         await client.edit_message(oldmessage, 'You have no trophies.')
 
@@ -30,11 +30,18 @@ async def get_top_spammer():
     user_days_in_chat = await get_user_days_in_chat()
     items = await db.fetch("""
         select
-        user_id,
-        count(*) as message_count
-         from message
-        where NOT bot AND content NOT LIKE '!%%'
-        group by user_id order by message_count desc limit 10
+            user_id,
+            count(*) as message_count
+        from 
+            message
+        where 
+            NOT bot AND content NOT LIKE '!%%'
+        group by 
+            user_id 
+        order by 
+            message_count 
+        desc 
+        limit 10
     """)
     if not items:
         return None
@@ -49,19 +56,29 @@ async def get_top_spammer():
 
 async def get_top_whosaidit():
     items = await db.fetch("""
-            with score as (
+            with score as 
+                (
+                select
+                    user_id,
+                    sum(case playeranswer when 'correct' then 1 else 0 end) as wins,
+                    sum(case playeranswer when 'wrong' then 1 else 0 end) as losses
+                from 
+                    whosaidit_stats_history
+                group by 
+                    user_id
+                )
             select
-                user_id,
-                sum(case playeranswer when 'correct' then 1 else 0 end) as wins,
-                sum(case playeranswer when 'wrong' then 1 else 0 end) as losses
-              from whosaidit_stats_history
-              group by user_id)
-            select
-             user_id
-            from score
-            join discord_user using (user_id)
-            where (wins + losses) >= 20
-            order by wins::float / (wins + losses) * 100 desc limit 1
+                user_id
+            from 
+                score
+            join 
+                discord_user using (user_id)
+            where 
+                (wins + losses) >= 20
+            order by 
+                wins::float / (wins + losses) * 100 
+            desc 
+            limit 1
     """)
     if not items:
         return None
@@ -70,7 +87,8 @@ async def get_top_whosaidit():
 
 async def get_top_whosaidit_score():
     items = await db.fetch("""
-        with week_score as (
+        with week_score as 
+        (
           select
             date_trunc('week', time) as dateadded,
             user_id,
@@ -81,15 +99,26 @@ async def get_top_whosaidit_score():
             100.0 * sum(case playeranswer when 'correct' then 1 else 0 end) / count(*) + least(20.0, sum(case playeranswer when 'correct' then 1 else 0 end) * 0.20) as score,
             max(100.0 * sum(case playeranswer when 'correct' then 1 else 0 end) / count (*) + least(20.0, sum(case playeranswer when 'correct' then 1 else 0 end) * 0.20)) over (partition by date_trunc('week', time)) as weeks_best_score,
             count(user_id) over (partition by date_trunc('week', time)) as players
-          from whosaidit_stats_history
-          group by user_id, date_trunc('week', time)
-          having count(*) >= 20
+          from 
+            whosaidit_stats_history
+          group by 
+            user_id, 
+            date_trunc('week', time)
+          having 
+            count(*) >= 20
         )
-        select user_id, score
-        from week_score
-        join discord_user using (user_id)
-          where not date_trunc('week', dateadded) = date_trunc('week', current_timestamp) and score = weeks_best_score and players >= 3
-        order by score desc limit 1
+        select 
+            user_id, 
+            score
+        from 
+            week_score
+        join 
+            discord_user using (user_id)
+        where 
+            not date_trunc('week', dateadded) = date_trunc('week', current_timestamp) and score = weeks_best_score and players >= 3
+        order by score 
+        desc 
+        limit 1
     """)
     if not items:
         return None
@@ -99,13 +128,17 @@ async def get_top_whosaidit_score():
 async def get_top_gambling_addict():
     items = await db.fetch("""
             select
-            count(*) as total,
-            user_id
+                count(*) as total,
+                user_id
             from message
-            where content like '!slots%' or content like '!blackjack%'
-            group by user_id
-            having count(*) >= 100
-            order by total desc
+            where 
+                content like '!slots%' or content like '!blackjack%'
+            group by 
+                user_id
+            having 
+                count(*) >= 100
+            order by 
+                total desc
             limit 1
     """)
     if not items:
@@ -117,19 +150,30 @@ async def get_least_toxic():
     items = await db.fetch("""
         with custommessage as (
             select
-            coalesce(name, m->'author'->>'username') as name,
-            user_id,
-            count(*) as message_count
-            from message
-            join discord_user using (user_id)
-            where NOT bot 
-            group by coalesce(name, m->'author'->>'username'), user_id)
+                coalesce(name, m->'author'->>'username') as name,
+                user_id,
+                count(*) as message_count
+            from 
+                message
+            join 
+                discord_user using (user_id)
+            where 
+                NOT bot 
+            group by coalesce(name, m->'author'->>'username'), user_id
+            )
         select
-        user_id
-         from message
-        join custommessage using (user_id)
-        where NOT bot and length(content) > 15 and message_count > 300 and name not like 'toxin'
-        group by user_id, message_count, name order by (count(*) / message_count::float) * 100 desc limit 1
+            user_id
+        from 
+            message
+        join 
+            custommessage using (user_id)
+        where 
+            NOT bot and length(content) > 15 and message_count > 300 and name not like 'toxin'
+        group by 
+            user_id, message_count, name 
+        order by (count(*) / message_count::float) * 100 
+        desc 
+        limit 1
     """)
     if not items:
         return None
