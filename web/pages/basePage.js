a2749427d4cc1399934f2640fdaf0697aaf98f95
Henry Heikkinen <rce@rce.fi>
2017-09-21 12:29:28 +0300

    Make website slightly better on mobile

--- web/pages/basePage.js
+++ web/pages/basePage.js
@@ -3,6 +3,7 @@ const React = require("react")
 const render = (page, applicationState, checksums) =>
   <html>
     <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <title>{page.pageTitle}</title>
       <script type="text/javascript" dangerouslySetInnerHTML={{__html: `window.CHECKSUMS = ${JSON.stringify(checksums)};`}}></script>
       <script async src={`bundle.js?checksum=${checksums.bundleJsChecksum}`}></script>
