7f9a8c977b76f9b500d77ab240dc89b5800e1f1f
Henry Heikkinen <rce@rce.fi>
2017-10-13 17:45:26 +0300

    pls work

--- journald-cloudwatch/build.sh
+++ journald-cloudwatch/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+set -o errexit
+
+apt install libsystemd-dev
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 rustup install stable
+
 cd journald-cloudwatch
 cargo build --release
-ls -l target/release
+
