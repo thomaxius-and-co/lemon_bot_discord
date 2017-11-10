4d2cf32280778db161d6ac4641d29ae9913afbc2
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-11-11 00:46:16 +0200

    Remove csco

--- src/faceit_commands.py
+++ src/faceit_commands.py
@@ -5,8 +5,8 @@ async def cmd_faceit_stats(client, message, faceit_nickname):
     if not faceit_nickname:
         await client.send_message(message.channel, "You need to specify a faceit nickname to search for.")
         return
-    csgo_elo, csco_elo, skill_level, csgo_name = await get_stats_from_api(client, message, faceit_nickname)
-    await client.send_message(message.channel, "Faceit stats for player nicknamed **%s**:\n**Name**: %s\n**CS:GO Elo**: %s\n**CS:CO Elo**: %s\n**Skill level**: %s" % (faceit_nickname, csgo_name, csgo_elo, csco_elo, skill_level))
+    csgo_elo, skill_level, csgo_name = await get_stats_from_api(client, message, faceit_nickname)
+    await client.send_message(message.channel, "Faceit stats for player nicknamed **%s**:\n**Name**: %s\n**CS:GO Elo**: %s\n**Skill level**: %s" % (faceit_nickname, csgo_name, csgo_elo, skill_level))
 
 async def get_stats_from_api(client, message, faceit_nickname):
     url = "https://api.faceit.com/api/nicknames/" + faceit_nickname
@@ -19,8 +19,8 @@ async def get_stats_from_api(client, message, faceit_nickname):
         csgo_name = result['payload']['csgo_name']
         skill_level = str(result['payload']['games']['csgo']['skill_level'])
         csgo_elo = str(result['payload']['games']['csgo']['faceit_elo'])
-        csco_elo = str(result['payload']['games']['csco']['faceit_elo'])
-        return csgo_elo, csco_elo, skill_level, csgo_name
+
+        return csgo_elo, skill_level, csgo_name
 
 def register(client):
     return {
