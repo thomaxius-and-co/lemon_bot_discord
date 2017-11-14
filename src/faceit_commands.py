3318c4155491260bce8e446463381882d66e8d0b
Henry Heikkinen <rce@rce.fi>
2017-11-14 07:49:48 +0200

    Fix faceit request logging
    Add response.status to the log entry. This gives us easy field to look
    for when finding abnormal behaviour, e.g. error codes 4XX, 5XX.
    
    Use response.text() instead of response.json() because the content
    might not always be JSON. If there is an error and the response is not
    valid JSON we still want to be able to log the response content and
    plain text is the only reliable way to do that.

--- src/faceit_commands.py
+++ src/faceit_commands.py
@@ -17,9 +17,9 @@ async def cmd_faceit_stats(client, message, faceit_nickname):
 async def get_user_stats_from_api(client, message, faceit_nickname):
     user_stats_url = "https://api.faceit.com/api/nicknames/" + faceit_nickname
     async with aiohttp.ClientSession() as session:
-        result = await session.get(user_stats_url)
-        result = await result.json()
-        log.info("GET %s %s", user_stats_url, result)
+        response = await session.get(user_stats_url)
+        log.info("GET %s %s %s", response.url, response.status, await response.text())
+        result = await response.json()
         if result['result'] == 'error':
             await client.send_message(message.channel, result['message'].replace(result['message'][0],result['message'][0].upper()) + ".") #Yes, I am this triggered by the first letter being a non-capital
             return None, None, None, None
