2b3da79f0b626c6f44e11bcb9016f4fd458c1639
Henry Heikkinen <rce@rce.fi>
2017-11-16 10:59:47 +0200

    Fix incorrect variable name

--- src/faceit_api.py
+++ src/faceit_api.py
@@ -4,7 +4,7 @@ import logger
 log = logger.get("FACEIT_API")
 
 async def ranking(guid, area="EU"):
-    url = "https://api.faceit.com/ranking/v1/globalranking/csgo/" + area + "/" + faceit_guid
+    url = "https://api.faceit.com/ranking/v1/globalranking/csgo/" + area + "/" + guid
     async with aiohttp.ClientSession() as session:
         response = await session.get(url)
         log.info("GET %s %s %s", response.url, response.status, await response.text())
