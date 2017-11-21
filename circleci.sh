f2a78ba8c81c2cfe1667e9b61fc2256382ffc112
Henry Heikkinen <rce@rce.fi>
2017-11-21 09:02:44 +0200

    Install missing zip command

--- circleci.sh
+++ circleci.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 apt-get update
-apt-get install -y build-essential git python-minimal python-pip findutils python3-minimal
+apt-get install -y \
+  zip \
+  build-essential git python-minimal python-pip findutils python3-minimal
 pip install -r ansible/requirements.txt
 pip install tox==2.5.0
 
