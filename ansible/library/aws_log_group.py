a90e23e4a1f94c596a9b243f011b26d74ac6e4c5
Henry Heikkinen <rce@rce.fi>
2017-11-23 23:22:44 +0200

    Prepare log group for archiver logs

---
+++ ansible/library/aws_log_group.py
@@ -0,0 +1,27 @@
+from ansible.module_utils.basic import AnsibleModule
+import boto3
+
+def main():
+    module = AnsibleModule(
+        argument_spec={
+            "name": {"type": "str", "required": True},
+        }
+    )
+
+    name = module.params["name"]
+    client = boto3.client("logs")
+
+    log_group = find_log_group(name)
+    if log_group is not None:
+        module.exit_json(changed=False, log_group=log_group)
+    else:
+        client.create_log_group(logGroupName=name)
+        log_group = find_log_group(name)
+        module.exit_json(changed=True, log_group=log_group)
+
+def find_log_group(name):
+    response = client.describe_log_groups(logGroupNamePrefix=name)
+    return head([g for g in response["logGroups"] if g["logGroupName"] == name])
+
+def head(xs):
+    return next(iter(xs), None)
