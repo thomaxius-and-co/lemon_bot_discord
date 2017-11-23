a57b6507c8fccfa234ed69b0a555b0859482b68e
Henry Heikkinen <rce@rce.fi>
2017-11-23 23:44:02 +0200

    Add missing parameter in aws_log_group module code

--- ansible/library/aws_log_group.py
+++ ansible/library/aws_log_group.py
@@ -11,15 +11,15 @@ def main():
     name = module.params["name"]
     client = boto3.client("logs")
 
-    log_group = find_log_group(name)
+    log_group = find_log_group(client, name)
     if log_group is not None:
         module.exit_json(changed=False, log_group=log_group)
     else:
         client.create_log_group(logGroupName=name)
-        log_group = find_log_group(name)
+        log_group = find_log_group(client, name)
         module.exit_json(changed=True, log_group=log_group)
 
-def find_log_group(name):
+def find_log_group(client, name):
     response = client.describe_log_groups(logGroupNamePrefix=name)
     return head([g for g in response["logGroups"] if g["logGroupName"] == name])
 
