fcbe1fd3e26b8ad9fb7f20e5d9e0603af763f680
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-12-20 03:08:14 +0200

    Maybe push actual fix that was supposed to be pushed

--- src/faceit_api.py
+++ src/faceit_api.py
@@ -17,6 +17,6 @@ async def user(nickname):
         response = await session.get(url)
         log.info("%s %s %s %s", response.method, response.url, response.status, await response.text())
         result = await response.json()
-        if result['result'] == 'error':
+        if result.get('result', None) == 'error':
             return None, result["message"].title()
         return result.get("payload", None), None
