c7f605fc2ec1b4f3e0808f42e2bb09e7cd1a0eba
Henry Heikkinen <rce@rce.fi>
2018-02-19 07:50:29 +0200

    Update muutto.py

--- src/muutto.py
+++ src/muutto.py
@@ -19,8 +19,8 @@ def delta_to_tuple(delta):
     return (days, hours, minutes, seconds)
 
 async def cmd_muutto(client, message, query):
-    muutto = to_utc(as_helsinki(datetime(2018, 03, 29, 12, 0)))
-    muuttoover = to_utc(as_helsinki(datetime(2018, 03, 30, 12, 0)))
+    muutto = to_utc(as_helsinki(datetime(2018, 3, 29, 12, 0)))
+    muuttoover = to_utc(as_helsinki(datetime(2018, 3, 30, 12, 0)))
     now = as_utc(datetime.now())
     now_to_last_muuttoover = now - muuttoover
 
