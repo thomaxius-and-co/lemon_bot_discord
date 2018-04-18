13f1106f90d56379d75573bc4c042d7ba4bea8db
Henry Heikkinen <rce@rce.fi>
2018-04-18 10:28:50 +0300

    Log error and event loop state if client.start() returns

--- src/run_lemon_bot.py
+++ src/run_lemon_bot.py
@@ -712,4 +712,9 @@ if __name__ == "__main__":
                    faceit_commands, muutto, statistics, crypto, status, emojicommands, lossimpsonquotes]:
         commands.update(module.register(client))
 
-    client.run(token)
+    try:
+        loop.run_until_complete(client.start(token))
+        raise Exception("client.start(...) returned (loop.is_closed() == {0})".format(loop.is_closed()))
+    except Exception as e:
+        new_loop = asyncio.new_event_loop()
+        new_loop.run_until_complete(util.log_exception(log))
