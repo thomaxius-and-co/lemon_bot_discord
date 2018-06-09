7ee09f1e1219285258ec59f7d8bc8fd27ddaeccd
Henry Heikkinen <rce@rce.fi>
2018-06-09 12:31:55 +0300

    Don't post errors to Discord from bot code
    All error logs are sent directly from Cloudwatch Logs to Discord.

--- src/util.py
+++ src/util.py
@@ -9,13 +9,9 @@ import logger
 
 log = logger.get("UTIL")
 
-webhook_url = os.environ.get("ERROR_CHANNEL_WEBHOOK", None)
-
 async def log_exception(error_log):
     err_str = traceback.format_exc()
     error_log.error(err_str)
-    if webhook_url is not None:
-        await post_exception(err_str)
 
     # If the error is 'Event loop is closed' we crash the bot and let it restart
     # A retarded solution to a retarded issue
@@ -23,24 +19,7 @@ async def log_exception(error_log):
     if error_message == 'Event loop is closed':
         msg = "Snipety snap! Event loop is closed! Committing sudoku in 3.. 2.. 1.."
         log.error(msg)
-        if webhook_url is not None:
-            await post_exception(msg)
         os._exit(0)
-
-async def post_exception(err_str):
-    data = {
-        "username": "Errors",
-        "icon_url": "https://rce.fi/error.png",
-        "text": "```" + err_str + "```",
-    }
-
-    async with aiohttp.ClientSession() as session:
-        r = await session.post(webhook_url + "/slack", data=json.dumps(data))
-        if r.status != 200:
-            log.error("Unknown webhook response {0}".format(r))
-            return
-
-        log.info("Posted error on channel")
 
 # Run discord.py coroutines from antoher thread
 def threadsafe(client, coroutine):
