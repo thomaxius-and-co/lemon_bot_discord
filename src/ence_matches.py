41dc9374832c2d83fde411639baa338ba68a91b0
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2018-10-07 00:53:14 +0300

    Atttempt to fix

--- src/ence_matches.py
+++ src/ence_matches.py
@@ -31,8 +31,12 @@ async def do_match_check(client):
 async def check_if_ence_day(client):
     log.info("Checking if match day")
     matches = MATCHES_DICT.get(datetime.datetime.now().date(), None)
-    if matches and (LAST_SPAMMED.date() != datetime.datetime.today().date()): #If already spammed today
-        await do_matchday_spam(client, matches)
+    if matches:
+        if LAST_SPAMMED:
+            if (LAST_SPAMMED.date() != datetime.datetime.today().date()): #If already spammed today
+                await do_matchday_spam(client, matches)
+        else:
+            await do_matchday_spam(client, matches)
     else:
         log.info('No matches today')
 
