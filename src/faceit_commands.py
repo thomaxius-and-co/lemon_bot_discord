0c7ec584c36080a53cea01946c949d1f684c6d23
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2019-03-29 15:56:07 +0200

    Fix re-implemented bug

--- src/faceit_commands.py
+++ src/faceit_commands.py
@@ -460,7 +460,7 @@ async def get_score_string(match):
     first_half_score = "%s-%s" % (match[0].get("teams")[0].get("team_stats").get("First Half Score"), match[0].get("teams")[1].get("team_stats").get("First Half Score"))
     second_half_score = "%s-%s" % (match[0].get("teams")[0].get("team_stats").get("Second Half Score"), match[0].get("teams")[1].get("team_stats").get("Second Half Score"))
     total_rounds = int(match[0].get("round_stats").get("Rounds"))
-    if total_rounds >= 30:
+    if total_rounds > 30:
         overtime_score = "%s-%s" % (
             match[0].get("teams")[0].get("team_stats").get("Overtime score"), match[0].get("teams")[1].get("team_stats").get("Overtime score"))
     if overtime_score:
