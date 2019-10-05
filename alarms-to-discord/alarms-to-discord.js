1464f568171df37b6d693d6d53973f5ebd14e0ee
Henry Heikkinen <rce@rce.fi>
2019-10-05 14:36:58 +0300

    Notify on any alarm state change

--- alarms-to-discord/alarms-to-discord.js
+++ alarms-to-discord/alarms-to-discord.js
@@ -5,13 +5,23 @@ const AWS = require("aws-sdk")
 AWS.config.update({ region: "eu-west-1" })
 const SecretsManager = new AWS.SecretsManager({ apiVersion: "2017-10-17" })
 
+const Colors = {
+  Red: 0xCC0000,
+  Green: 0x009933,
+  Yellow: 0xE07700,
+  Gray: 0xE0E0E0,
+}
+
 exports.handler = async function(event) {
   const DISCORD_WEBHOOK_URL = await fetchSecretWebhookUrl()
   console.log("Handling event", JSON.stringify(event, null, 2))
   const messages = event.Records.map(_ => JSON.parse(_.Sns.Message))
 
-  const messagePayloads = messages.map(({AlarmName, NewStateReason}) => {
-    return {embeds: [{title: AlarmName, description: NewStateReason, color: 0xff0000}]}
+  const messagePayloads = messages.map(({AlarmName, NewStateReason, NewStateValue}) => {
+    const color = NewStateValue == "ALARM" ? Colors.Red :
+      NewStateValue == "INSUFFICIENT_DATA" ? Colors.Yellow :
+      NewStateValue == "OK" ? Colors.Green : Colors.Gray
+    return {embeds: [{title: AlarmName, description: NewStateReason, color}]}
   })
 
   for (const data of messagePayloads) {
