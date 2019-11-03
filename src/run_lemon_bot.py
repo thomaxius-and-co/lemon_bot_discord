80204de4aa0af1a0759a2025b896b21841114c42
Henry Heikkinen <rce@rce.fi>
2019-11-03 17:20:51 +0200

    Use asyncio.run() that became stable in Python 3.8

--- src/run_lemon_bot.py
+++ src/run_lemon_bot.py
@@ -732,6 +732,4 @@ async def on_ready():
     await status.main(client)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-
+    asyncio.run(main())
