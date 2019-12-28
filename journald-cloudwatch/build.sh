a07abc6f2089425d9f39010a979cc85515b02e00
Henry Heikkinen <rce@rce.fi>
2019-12-28 19:42:14 +0200

    Pin rust version to 1.39.0 instead of latest stable

--- journald-cloudwatch/build.sh
+++ journald-cloudwatch/build.sh
@@ -5,7 +5,7 @@ apt-get -q install -y curl pkg-config libsystemd-dev libssl-dev
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
-rustup install stable
+rustup install 1.39.0
 
 cd journald-cloudwatch
 cargo test --release
