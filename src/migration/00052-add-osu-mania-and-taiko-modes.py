ecb303a78ead7a510de4d9355b5f743a344c0614
Henry Heikkinen <rce@rce.fi>
2020-09-13 17:05:34 +0300

    Add missing migration script

---
+++ src/migration/00052-add-osu-mania-and-taiko-modes.py
@@ -0,0 +1,11 @@
+async def exec(log, tx):
+  await tx.execute("""
+    INSERT INTO osugamemode (osugamemode_id, name) VALUES
+    ('TAIKO', 'osu!taiko'),
+    ('CATCH', 'osu!catch')
+  """)
+  for mode in ['TAIKO', 'CATCH']:
+    await tx.execute("""
+      INSERT INTO osupp (osuuser_id, osugamemode_id, pp, rank, changed)
+      SELECT osu_user_id, $1, NULL, NULL, current_timestamp FROM osu_pp
+    """, mode)
