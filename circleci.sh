165f711d44304b41af45af7fd0eeaf6b1450235e
Henry Heikkinen <rce@rce.fi>
2020-12-12 20:27:45 +0200

    Run tests on CircleCI

--- circleci.sh
+++ circleci.sh
@@ -20,8 +20,24 @@ function main {
   # Workaround for issues with pip/python 2 and UTF-8
   export LC_ALL="C.UTF-8"
 
+  run_tests
   ./journald-cloudwatch/build.sh
   ./deploy.sh
+}
+
+function run_tests {
+  set_python_version
+  install_python_dependencies
+
+  cd "$repo"
+  DATABASE_HOST="localhost" \
+  DATABASE_PORT="5432" \
+  DATABASE_NAME="lemon" \
+  DATABASE_USERNAME="lemon" \
+  DATABASE_PASSWORD="lemon" \
+  REDIS_HOST="localhost" \
+  REDIS_PORT="6379" \
+  python -m pytest
 }
 
 function install_pyenv {
