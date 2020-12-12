f51966c98a50bdab5fb6aa48138b28c069b79df7
Henry Heikkinen <rce@rce.fi>
2020-12-12 20:22:20 +0200

    Give circleci.sh the same boilerplate treatment as all other scripts

--- circleci.sh
+++ circleci.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
+source "$( dirname "${BASH_SOURCE[0]}" )/scripts/common-functions.sh"
 
-set -o errexit
+function main {
+  # For add-apt-repository
+  apt-get -q update
+  apt-get -q install -y software-properties-common
 
-# For add-apt-repository
-apt-get -q update
-apt-get -q install -y software-properties-common
+  add-apt-repository -y ppa:deadsnakes/ppa
+  apt-get -q update
 
-add-apt-repository -y ppa:deadsnakes/ppa
-apt-get -q update
+  apt-get -q install -y \
+    python3.9 python3.9-dev \
+    libxml2-dev libxslt1-dev zlib1g-dev \
+    curl zip \
+    build-essential git python-minimal python-pip findutils python3-minimal python3-dev
 
-apt-get -q install -y \
-  python3.9 python3.9-dev \
-  libxml2-dev libxslt1-dev zlib1g-dev \
-  curl zip \
-  build-essential git python-minimal python-pip findutils python3-minimal python3-dev
+  install_pyenv
 
-# Install pyenv for choosing any specific Python vversion
-curl https://pyenv.run | bash
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+  # Workaround for issues with pip/python 2 and UTF-8
+  export LC_ALL="C.UTF-8"
 
-# Workaround for issues with pip/python 2 and UTF-8
-export LC_ALL="C.UTF-8"
+  ./journald-cloudwatch/build.sh
+  ./deploy.sh
+}
 
-./journald-cloudwatch/build.sh
-./deploy.sh
+function install_pyenv {
+  curl https://pyenv.run | bash
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+}
+
+main "$@"
