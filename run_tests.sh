f12549b1761cd96c7353b327d6787ca18376cd17
Henry Heikkinen <rce@rce.fi>
2020-12-12 21:18:10 +0200

    Revert "Update discord.py, aiohttp, aioredis to newer Python 3.9 friendly versions"
    This reverts commit 39d051c919f173661c60e0cbcaec50b92d91444e.

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,7 @@ source "$( dirname "${BASH_SOURCE[0]}" )/scripts/common-functions.sh"
 function main {
   cd "$repo"
   set_python_version
+  #python -m pipenv install --dev pytest-watch
   install_python_dependencies
 
   source "$repo/secrets"
