428bf8776100969ca7fb6f2c74fc4388512e627b
Henry Heikkinen <rce@rce.fi>
2020-12-12 21:19:28 +0200

    Run apt-get install non-interactively when installing pyenv dependencies

--- circleci.sh
+++ circleci.sh
@@ -34,7 +34,7 @@ function run_tests {
 
 function install_pyenv {
   # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-  apt-get install --no-install-recommends \
+  apt-get -q install -y --no-install-recommends \
     make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
     libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev \
     libxmlsec1-dev libffi-dev liblzma-dev
