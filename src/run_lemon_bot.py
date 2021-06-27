8fd2eaed2e9c9b35add211bc6b2c71ec53ff19fe
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2021-06-27 23:36:50 +0300

    enään

--- src/run_lemon_bot.py
+++ src/run_lemon_bot.py
@@ -751,6 +751,8 @@ async def on_message(message):
             await message.channel.send("Heh heh, kohta voidaankin taas kätellä, kun on nämä rokotteet")
         elif "orona" in content.lower():
             await message.channel.send("Heh heh, nyt ei voidakaan kätellä, kun on tämä korona")
+        elif "enään" in content.lower():
+            await message.channel.send("Yleiskielessä käytetään muotoa enää eikä länsimurteissa tavallista muotoa enään.")
         elif len(content) > 1000 and "suihkuun" in pasta.pastas:
             await message.channel.send(pasta.pastas.get("suihkuun"))
         if len(message.content) < 10 and message.channel.id == 789916648483717130 and message.content.isdigit():
